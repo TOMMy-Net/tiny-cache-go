5f8208e0246bd6a96b7a887a82be1fa8f28ad033
Timofey1234567890 <74640949+Timofey1234567890@users.noreply.github.com>
2024-01-19 04:11:18 +0300

    add new

--- cache/memc.go
+++ cache/memc.go
@@ -1,1 +1,41 @@
 package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+    sync.RWMutex
+    defaultExpiration time.Duration
+    cleanupInterval   time.Duration
+    items             map[string]Item
+}
+
+type Item struct {
+    Value      interface{}
+    Created    time.Time
+    Expiration int64
+}
+
+func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
+
+    items := make(map[string]Item)
+
+    cache := Cache{
+        items:             items,
+        defaultExpiration: defaultExpiration,
+        cleanupInterval:   cleanupInterval,
+    }
+
+    // Если интервал очистки больше 0, запускаем GC (удаление устаревших элементов)
+    if cleanupInterval > 0 {
+       cache.StartGC() // данный метод рассматривается ниже
+    }
+
+    return &cache
+}
+
+func (c *Cache) StartGC()  {
+	
+}
